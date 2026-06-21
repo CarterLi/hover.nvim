a29788de791c66142a0bba1cc7a547212eb417bf
Carter Li <CarterLi@users.noreply.github.com>
2026-06-21 10:14:37 +0800

    fix: hover action result handling (#120)

--- lua/hover/actions.lua
+++ lua/hover/actions.lua
@@ -200,7 +200,11 @@ local function run_provider(provider, providers, bufnr, popts)
   local result = async.await(2, provider.execute, {
     bufnr = bufnr,
     pos = popts.pos or api.nvim_win_get_cursor(0),
-  }) or { lines = { 'No result' } }
+  })
+
+  if not result then
+    return false
+  end
 
   async.scheduler()
   show_hover(provider.id, providers, config, result, opts)
